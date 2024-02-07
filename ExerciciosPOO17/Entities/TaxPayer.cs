6da9c68b82789de6a3fb33f4a444957c233021fc
João Victor <jvaarantes@gmail.com>
2024-02-07 09:58:50 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosPOO17/Entities/TaxPayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosPOO17.Entities
+{
+    abstract class TaxPayer
+    {
+        public String Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            this.Name = name;
+            this.AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+
+    }
+}
---
+++ ExerciciosPOO17/Program.cs
@@ -0,0 +1,74 @@
+﻿using ExerciciosPOO17.Entities;
+
+namespace ExerciciosPOO17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            int aux = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < aux; i++)
+            {
+                Console.WriteLine($"Tax payer #{i + 1} data:");
+
+                Console.Write("Individual or company (i/c)? ");
+                char Situation = char.Parse(Console.ReadLine());
+
+                String Name;
+                double AnualIncome;
+
+
+                if (Situation == 'i')
+                {
+                    Console.Write("Nome: ");
+                    Name = Console.ReadLine();
+
+                    Console.Write("Anual income: ");
+                    AnualIncome = double.Parse(Console.ReadLine());
+
+                    Console.Write("Health expenditures: ");
+                    double HealthExpenditures = double.Parse(Console.ReadLine());
+
+                    list.Add(new Individual(Name, AnualIncome, HealthExpenditures));
+                }
+                else if (Situation == 'c')
+                {
+                    Console.Write("Nome: ");
+                    Name = Console.ReadLine();
+
+                    Console.Write("Anual income: ");
+                    AnualIncome = double.Parse(Console.ReadLine());
+
+                    Console.Write("Number of employees: ");
+                    int NumberOfEmployees = int.Parse(Console.ReadLine());
+
+                    list.Add(new Company(Name, AnualIncome, NumberOfEmployees));
+                }
+                else
+                {
+                    Console.WriteLine("Digite uma opção válida! :)");
+                }                
+            }
+
+            Console.WriteLine("\nTax Paid:\n");
+
+            double Ammount = 0;
+
+            foreach (TaxPayer payer in list)
+            {
+                Console.WriteLine($"{payer.Name}: ${payer.Tax().ToString("F2")}");
+                Ammount += payer.Tax();
+            }
+
+            Console.WriteLine($"\nTOTAL TAXES: {Ammount}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
