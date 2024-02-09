27ad9c07edb97daf9228098286467c481849c26c
João Victor <jvaarantes@gmail.com>
2024-02-09 11:06:36 -0300

    Tentei fazer uma tratativa de erro para cada input. Ficou feio mas foi bom para o meu aprendizado.

---
+++ ExerciciosPOO17/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosPOO17.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(String message) : base(message) { }
+
+    }
+}
--- ExerciciosPOO17/Program.cs
+++ ExerciciosPOO17/Program.cs
@@ -9,50 +9,178 @@ namespace ExerciciosPOO17
 
             List<TaxPayer> list = new List<TaxPayer>();
 
-            Console.Write("Enter the number of tax payers: ");
-            int aux = int.Parse(Console.ReadLine());
+            int aux = 0;
+            bool TaErrado = false;
 
+            while (!TaErrado)
+            {
+                try
+                {
+                    Console.Write("Enter the number of tax payers: ");
+                    aux = int.Parse(Console.ReadLine());
+                }
+
+                catch (FormatException e)
+                {
+                    TaErrado = false;
+
+                    Console.WriteLine($"Você digitou um tipo de valor errado. :( \n{e.Message}");
+
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine($"Você digitou um tipo de valor errado. :( \n{e.Message}");
+                }
+                finally
+                {
+                    if (!TaErrado)
+                    {
+                        Console.WriteLine("Aperte alguma tecla para tentar novamente.");
+
+                        Console.ReadKey();
+
+                        Console.Clear();
+                    }
+
+                    if (aux == 0 && TaErrado)
+                    {
+                        throw new FormatException();
+
+                    }
+                }
+
+            }
             for (int i = 0; i < aux; i++)
             {
-                Console.WriteLine($"Tax payer #{i + 1} data:");
-
-                Console.Write("Individual or company (i/c)? ");
-                char Situation = char.Parse(Console.ReadLine());
-
                 String Name;
                 double AnualIncome;
+                double HealthExpenditures;
+                char Situation = 'a';
+
+                Console.WriteLine($"Tax payer #{i + 1} data:");
 
 
+
+                while (!TaErrado)
+                {
+                    try
+                    {
+                        Console.Write("Individual or company (i/c)? ");
+                        Situation = char.Parse(Console.ReadLine());
+
+                    }
+                    catch (FormatException e)
+                    {
+                        TaErrado = false;
+
+                        Console.WriteLine($"Você digitou um tipo de valor errado. :( \n{e.Message}");
+
+                    }
+                    catch (ArgumentNullException e)
+                    {
+                        Console.WriteLine($"Você digitou um tipo de valor errado. :( \n{e.Message}");
+                    }
+                    finally
+                    {
+                        if (!TaErrado)
+                        {
+                            Console.WriteLine("Aperte alguma tecla para tentar novamente.");
+
+                            Console.ReadKey();
+
+                            Console.Clear();
+                        }
+                    }
+                }
                 if (Situation == 'i')
                 {
-                    Console.Write("Nome: ");
-                    Name = Console.ReadLine();
+                    while (!TaErrado)
+                    {
+                        try
+                        {
+                            Console.Write("Nome: ");
+                            Name = Console.ReadLine();
 
-                    Console.Write("Anual income: ");
-                    AnualIncome = double.Parse(Console.ReadLine());
+                            Console.Write("Anual income: ");
+                            AnualIncome = double.Parse(Console.ReadLine());
 
-                    Console.Write("Health expenditures: ");
-                    double HealthExpenditures = double.Parse(Console.ReadLine());
+                            Console.Write("Health expenditures: ");
+                            HealthExpenditures = double.Parse(Console.ReadLine());
 
-                    list.Add(new Individual(Name, AnualIncome, HealthExpenditures));
+                            list.Add(new Individual(Name, AnualIncome, HealthExpenditures));
+                        }
+                        catch (FormatException e)
+                        {
+                            TaErrado = false;
+
+                            Console.WriteLine($"Você digitou um tipo de valor errado. :( \n{e.Message}");
+
+                        }
+                        catch (ArgumentNullException e)
+                        {
+                            Console.WriteLine($"Você digitou um tipo de valor errado. :( \n{e.Message}");
+                        }
+                        finally
+                        {
+                            if (!TaErrado)
+                            {
+                                Console.WriteLine("Aperte alguma tecla para tentar novamente.");
+
+                                Console.ReadKey();
+
+                                Console.Clear();
+                            }
+                        }
+                    }
                 }
                 else if (Situation == 'c')
                 {
-                    Console.Write("Nome: ");
-                    Name = Console.ReadLine();
+                    while (!TaErrado)
+                    {
 
-                    Console.Write("Anual income: ");
-                    AnualIncome = double.Parse(Console.ReadLine());
+                        try
+                        {
 
-                    Console.Write("Number of employees: ");
-                    int NumberOfEmployees = int.Parse(Console.ReadLine());
+                            Console.Write("Nome: ");
+                            Name = Console.ReadLine();
 
-                    list.Add(new Company(Name, AnualIncome, NumberOfEmployees));
+                            Console.Write("Anual income: ");
+                            AnualIncome = double.Parse(Console.ReadLine());
+
+                            Console.Write("Number of employees: ");
+                            int NumberOfEmployees = int.Parse(Console.ReadLine());
+
+                            list.Add(new Company(Name, AnualIncome, NumberOfEmployees));
+
+                        }
+                        catch (FormatException e)
+                        {
+                            TaErrado = false;
+
+                            Console.WriteLine($"Você digitou um tipo de valor errado. :( \n{e.Message}");
+
+                        }
+                        catch (ArgumentNullException e)
+                        {
+                            Console.WriteLine($"Você digitou um tipo de valor errado. :( \n{e.Message}");
+                        }
+                        finally
+                        {
+                            if (!TaErrado)
+                            {
+                                Console.WriteLine("Aperte alguma tecla para tentar novamente.");
+
+                                Console.ReadKey();
+
+                                Console.Clear();
+                            }
+                        }
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Digite uma opção válida! :)");
-                }                
+                }
             }
 
             Console.WriteLine("\nTax Paid:\n");
